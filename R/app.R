e1dc474d8fe3d062cc9119bbfef6547134913ab5
Joe Brew <joebrew@gmail.com>
2020-05-11 14:38:22 +0200

    initial

--- R/app.R
+++ R/app.R
@@ -7,7 +7,6 @@
 #' @import leaflet
 #' @import shiny
 #' @import ggplot2
-#' @import shinyMobile
 app_ui <- function(request) {
   options(scipen = '999')
   
@@ -15,18 +14,12 @@ app_ui <- function(request) {
     mobile_golem_add_external_resources(),
     
     dashboardPage(
-      dashboardHeader (title = "Databrew Dashboard"),
+      dashboardHeader (title = "SAINT"),
       dashboardSidebar(
         sidebarMenu(
           menuItem(
             text="Main",
             tabName="main"),
-          menuItem(
-            text="Charts",
-            tabName="charts"),
-          menuItem(
-            text = 'Widgets',
-            tabName = 'widgets'),
           menuItem(
             text = 'About',
             tabName = 'about')
@@ -40,54 +33,12 @@ app_ui <- function(request) {
             tabName="main",
             navbarPage(title = '',
                        collapsible = TRUE,
-                       tabPanel(title = "Xing's test page",
-                                fluidRow(
-                                  shinydashboard::box(title = 'This is another box',
-                                                      width = 6,
-                                                      status = 'warning',
-                                                      collapsible = TRUE,
-                                                      footer = 'This is a footer',
-                                                      plotOutput('plot1')),
-                                  column(6,
-                                         h1('Big heading (h1)'),
-                                         h2('Less big heading (h2)'),
-                                         h3('Sort of big heading (h3)'),
-                                         h4('Not so big heading (h4)'),
-                                         h5('Small heading (h5)'),
-                                         h6('Heading w/ background (h6)'))
-                                ),
-                                fluidRow(
-                                  column(4,
-                                         h4('A bunch of inputs'),
-                                         p(selectInput('abc', 'Pick a place', choices = c('Home', 'Away', 'In-between')),
-                                           radioButtons('xyz', 'What do you like?', choices = c('Ice cream', 'Pizza', 'Both', 'Neither', 'Ice pizza')),
-                                           dateRangeInput('aslk', 'Date range', start = Sys.Date() - 20, end = Sys.Date() - 5),
-                                           actionButton('action', 'This is a button', icon = icon('download')),
-                                           sliderInput('lakjaasa', 'This is a slider', min = 0, max = 100, value = 25),
-                                           textInput('qwer', 'This is some text input'))),
-                                  column(4,
-                                         h4('Here is some regular text'),
-                                         p('This is normal (ie, p) text, This is normal (ie, p) text, This is normal (ie, p) text,
-                                     This is normal (ie, p) text, This is normal (ie, p) text, This is normal (ie, p) text'),
-                                         helpText('This is "help text"')),
-                                  shinydashboard::box(title = 'This is another box',
-                                                      width = 4,
-                                                      status = 'info',
-                                                      collapsible = TRUE,
-                                                      footer = 'This is a footer',
-                                                      leaflet::leafletOutput('l1')
-                                  )
-                                )
+                       tabPanel(title = "Overview",
+                                fluidPage()
                        ),
-                       navbarMenu("Tab A",
-                                  tabPanel("Dropdown A"),
-                                  tabPanel("Dropdown B")),
-                       tabPanel('Tab B'),
-                       tabPanel('Tab C'),
-                       tabPanel('Tab D'),
-                       navbarMenu("Tab E",
-                                  tabPanel("Dropdown 1"),
-                                  tabPanel("Dropbdown 2"))
+                       navbarMenu("Data",
+                                  tabPanel("Raw data"),
+                                  tabPanel("Meta-data"))
                        
             )
           ),
@@ -95,7 +46,7 @@ app_ui <- function(request) {
             tabName = 'about',
             fluidPage(
               fluidRow(
-                div(img(src='logo_clear.png', align = "center"), style="text-align: center;"),
+                div(img(src='www/logo_clear.png', align = "center"), style="text-align: center;"),
                 h4('Built in partnership with ',
                    a(href = 'http://databrew.cc',
                      target='_blank', 'Databrew'),
@@ -176,7 +127,13 @@ mobile_golem_add_external_resources <- function(){
 ##################################################
 #' @import shiny
 #' @import leaflet
+#' @import yaml
 app_server <- function(input, output, session) {
+  
+  # Get data
+  get_data(data_file = paste0(getwd(), '/data.csv'),
+           user = yaml::read_yaml('credentials/credentials.yaml')$user,
+           password = yaml::read_yaml('credentials/credentials.yaml')$password)
   
 }
 
